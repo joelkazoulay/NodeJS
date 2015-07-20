74dd00e8e9af56a68e601f4ba7e34aebb6a7ea99
Joel Azoulay <joelkazoulay@gmail.com>
2015-07-20 10:56:00 -0600

    finished content negotiation

---
+++ content-negotiation/db.js
@@ -0,0 +1,11 @@
+var users = [];
+
+function createUserObj(name) {
+	return { name: name };
+}
+
+users.push('Tobi');
+users.push('Loki');
+users.push('Jane');
+
+module.exports = users;
--- content-negotiation/index.js
+++ content-negotiation/index.js
@@ -2,6 +2,9 @@ var express = require('express'),
 	app = module.exports = express(),
 	users = require('./db');
 
+	console.log('Users: ' + users);
+	console.log('Users var data type: ' + typeof users);
+
 //so either you can deal with different types of formatting
 //for expected response in index.js
 app.get('/', function(req, res) {
@@ -10,27 +13,30 @@ app.get('/', function(req, res) {
 		text: text,
 		json: json
 	});
-});
 
-function html() {
-	res.send(constructUl().join());
+	function html() {
+		function constructUl() {
+			var text = constructText();
+			console.log(text);
+			return '<ul>' + users.map(text);
+		}
 
-	function constructUl() {
-		return '<ul>' + users.map(constructText);
+		res.send(constructUl().join());
 	}
-}
 
-function text() {
-	res.send(users.map(constructText).join(''));
-}
+	function text() {
+		res.send(users.map(constructText).join(''));
+	}
 
-function json() {
-	res.json(users);
-}
+	function json() {
+		res.json(users);
+	}
 
-function constructText(user) {
-	return ' - ' + user.name + '\n';
-}
+	function constructText(user) {
+		console.
+		return ' - ' + user.name + '\n';
+	}
+});
 
 // or you could write a tiny middleware like
 // this to add a layer of abstraction 
@@ -47,6 +53,6 @@ function constructText(user) {
 // app.get('/users', format('./users'));
 
 if(!module.parent) {
-	app.listen(3900);
+	app.listen(3000);
 	console.log('Express started on port 3000');
 }
